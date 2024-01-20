686a423006ff6555d3fc7dc88e982bed925fb33f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-01-20 16:13:34 +0200

    Add icons & minor refactor

--- client/src/components/FilterMenu.tsx
+++ client/src/components/FilterMenu.tsx
@@ -2,24 +2,43 @@ import DatePostingList from "./DatePostingList";
 import LocationMenu from "./LocationList";
 import PriceList from "./PriceList";
 import SeniorityList from "./SeniorityList";
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { CiCalendarDate } from "react-icons/ci";
 
 const FilterMenu = () => {
   return (
     <div className="hidden mt-12 lg:flex lg:flex-col lg:bg-white w-1/3 xl:w-1/4 min-h-screen rounded-sm px-4 py-4">
       <div className="flex flex-col gap-3">
-        <h1 className="font-semibold">Location</h1>
+        <div className="flex items-center gap-2 text-blue-600">
+          <CiLocationOn className="text-2xl" />
+          <h1 className="font-semibold">Location</h1>
+        </div>
         <LocationMenu />
       </div>
-      <div className="flex flex-col gap-3 mt-7">
-        <h1 className="font-semibold">Seniority</h1>
+      <hr className="mt-4" />
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex items-center gap-2 text-blue-600">
+          <MdOutlineWorkOutline className="text-xl" />
+          <h1 className="font-semibold">Seniority</h1>
+        </div>
         <SeniorityList />
       </div>
-      <div className="flex flex-col gap-3 mt-7">
-        <h1 className="font-semibold">Salary</h1>
+      <hr className="mt-4" />
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex items-center gap-2 text-blue-600">
+          <MdOutlineAttachMoney className="text-2xl" />
+          <h1 className="font-semibold">Salary</h1>
+        </div>
         <PriceList />
       </div>
-      <div className="flex flex-col gap-3 mt-7">
-        <h1 className="font-semibold">Date of posting</h1>
+      <hr className="mt-4" />
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex items-center gap-2 text-blue-600">
+          <CiCalendarDate className="text-2xl" />
+          <h1 className="font-semibold">Date of posting</h1>
+        </div>
         <DatePostingList />
       </div>
     </div>
