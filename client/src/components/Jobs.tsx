db7e9b10ad21c7513fd5a474572e109da9cce49a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-01-23 11:51:05 +0200

    Add search functionality

--- client/src/components/Jobs.tsx
+++ client/src/components/Jobs.tsx
@@ -1,16 +1,55 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CiLocationOn, CiClock2, CiCalendarDate } from "react-icons/ci";
 import { PiMoneyLight } from "react-icons/pi";
 import data from "../mock/data.json";
+import { resetFilters } from "../redux/filters/filterSlice";
+import { useDispatch } from "react-redux";
 
-const Jobs = () => {
+type Job = {
+  id: string;
+  company: string;
+  jobTitle: string;
+  location: string;
+  logo: string;
+  employmentType: string;
+  salaryRange: string;
+  datePosted: string;
+};
+
+interface JobsProps {
+  searchValues: {
+    search: string;
+    location: string;
+  };
+}
+
+const Jobs: React.FC<JobsProps> = ({ searchValues }: any) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { search, location } = searchValues;
+
+  const filteredJobs = data.filter((job: Job) => {
+    const searchMatch =
+      job.jobTitle.toLowerCase().includes(search.toLowerCase()) ||
+      job.company.toLowerCase().includes(search.toLowerCase());
+
+    const locationMatch = job.location
+      .toLowerCase()
+      .includes(location.toLowerCase());
+
+    return searchMatch && locationMatch;
+  });
 
   return (
     <div className="mt-12 flex flex-col bg-white w-full min-h-screen rounded-sm px-4 py-4 gap-4">
-      {data.map((job) => (
+      {filteredJobs.map((job) => (
         <div
-          onClick={() => navigate(`/job/${job.id}`)}
+          key={job.id}
+          onClick={() => {
+            navigate(`/job/${job.id}`);
+            dispatch(resetFilters());
+          }}
           className="flex flex-row gap-6 p-3.5 rounded-md bg-gradient-to-r from-white to-blue-100 cursor-pointer
          hover:shadow-lg transition duration-300 ease-in-out py-6"
         >
