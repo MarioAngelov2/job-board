64167601deac160c28eaa638a5631838f6eacb35
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-01-23 11:49:41 +0200

    Add redux wrapper

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 import RootLayout from "./components/RootLayout";
 import DashboardLayout from "./components/DashboardLayout";
@@ -31,6 +33,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
