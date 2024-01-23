2071230f2357be478a391c7aa1ecbd973f9507b7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-01-23 11:50:46 +0200

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -88,7 +88,7 @@ const Home = () => {
       {/* FILTER MENU */}
       <div className="lg:flex lg:flex-row lg:gap-6">
         <FilterMenu />
-        <Jobs />
+        <Jobs searchValues={form.getValues()}/>
       </div>
     </MaxWidthWrapper>
   );
