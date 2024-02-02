7a158cb2f1c64e312751454d30af8299913ce261
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-02 13:45:30 +0200

    Use job validation

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createJob } from "../controllers/createJob";
 import { getJobs } from "../controllers/getJobs";
+import { addJobSchema, jobValidation } from "../middleware/jobValidation";
 
 const router = express.Router();
 
-router.post("/createJob", createJob);
+router.post("/createJob", jobValidation(addJobSchema), createJob);
 router.get("/getJobs", getJobs);
 
 export default router;
