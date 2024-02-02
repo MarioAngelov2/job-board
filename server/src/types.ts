320d82ce2a95a773caa8d317738fb4aa15a17c6c
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-02 12:52:44 +0200

    Create types file

---
+++ server/src/types.ts
@@ -0,0 +1,16 @@
+export type Job = {
+  id: string;
+  company: string;
+  jobTitle: string;
+  location: string;
+  employmentType: string;
+  salaryRange: string;
+  salaryType: string;
+  seniorityLevel: string;
+  seniorityType: string;
+  aboutUs: string;
+  tasks: string;
+  requirements: string[];
+  benefits: string[];
+  finalWords: string;
+};
