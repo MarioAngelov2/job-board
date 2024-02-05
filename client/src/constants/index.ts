d066492c46944853fefb2c36ea42676f638e8407
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-05 20:12:07 +0200

    Create constants file

---
+++ client/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const seniorityOptions = [
+  { value: "intern", label: "Intern" },
+  { value: "junior", label: "1-2 years experience" },
+  { value: "mid", label: "2-5 years experience" },
+  { value: "senior", label: "+5 years experience" },
+  { value: "teamLead", label: "Team Lead" },
+];
