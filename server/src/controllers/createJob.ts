08947308c9c1c782f84f527df5605776d5badde7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-05 20:12:24 +0200

    Update createJob.ts

--- server/src/controllers/createJob.ts
+++ server/src/controllers/createJob.ts
@@ -3,6 +3,8 @@ import { addJob } from "../services/createJob";
 import { v4 as uuidv4 } from "uuid";
 
 export const createJob = async (req: Request, res: Response) => {
+  console.log(req.body);
+
   try {
     const {
       company,
