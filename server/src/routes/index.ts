e57c28180936bb96c6bc0c0726b98f0c75c4406e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-07 08:54:03 +0200

    Create route for uploading images

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { createJob } from "../controllers/createJob";
 import { getJobs } from "../controllers/getJobs";
+import { uploadCompanyLogo } from "../controllers/images";
 import { addJobSchema, jobValidation } from "../middleware/jobValidation";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/jobs/createJob",  createJob);
+router.post("/jobs/createJob", createJob);
 router.get("/jobs/getJobs", getJobs);
+router.post("/images/upload-image", multer().single("image"), uploadCompanyLogo)
 
 export default router;
 
