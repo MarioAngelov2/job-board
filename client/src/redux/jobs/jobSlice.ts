3e7342af8b5e5e8efbb89ee11f1365b610ea5e05
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-04 16:58:52 +0200

    Add fetch jobs thunk & refactor

--- client/src/redux/jobs/jobSlice.ts
+++ client/src/redux/jobs/jobSlice.ts
@@ -7,22 +7,49 @@ type Job = {
   location: string;
   employmentType: string;
   salaryRange: string;
-  salaryType: string;
   seniorityLevel: string;
-  seniorityType: string;
   aboutUs: string;
   tasks: string;
   requirements: string[];
   benefits: string[];
-  companyLogo: string;
+  companyLogo: string | null;
 };
 
-export const addJob = createAsyncThunk("jobs/addJob", async (data: any) => {
+type FetchedJob = {
+  jobTitle: string;
+  employmentType: string;
+  salaryRange: string;
+  salaryType: string;
+  seniorityLevel: string;
+  seniorityType: string;
+  aboutUs: string;
+  datePosted: string;
+  finalWords: string;
+  id: string;
+  company: string;
+  location: string;
+  tasks: string;
+  requirements: string[];
+  benefits: string[];
+  logo: string;
+}
+
+const initialState = {
+  addJob: {
+    jobs: [] as Job[],
+    loading: false,
+    error: null as string | null,
+  },
+  fetchJobs: {
+    jobs: [] as Job[],
+    loading: false,
+    error: null as string | null,
+  }
+};
+
+export const addJob = createAsyncThunk("jobs/addJob", async (data: Job) => {
   try {
-    const res = await axios.post(
-      "http://localhost:8080/jobs/createJob",
-      data
-    );
+    const res = await axios.post("http://localhost:8080/jobs/createJob", data);
 
     return res.data;
   } catch (error) {
@@ -30,11 +57,15 @@ export const addJob = createAsyncThunk("jobs/addJob", async (data: any) => {
   }
 });
 
-const initialState = {
-  jobs: [] as Job[],
-  loading: false,
-  error: null as string | null,
-};
+export const fetchJobs = createAsyncThunk("jobs/fetchJobs", async () => {
+  try {
+    const res = await axios.get("http://localhost:8080/jobs/getJobs");
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const jobSlice = createSlice({
   name: "jobs",
@@ -43,18 +74,34 @@ const jobSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(addJob.pending, (state) => {
-        state.loading = true;
-        state.error = null;
+        state.addJob.loading = true;
+        state.addJob.error = null;
       })
       .addCase(addJob.fulfilled, (state, action) => {
-        state.loading = false;
-        state.jobs.push(action.payload);
+        state.addJob.loading = false;
+        state.addJob.jobs.push(action.payload);
       })
       .addCase(addJob.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "Failed to add job.";
-      });
+        state.addJob.loading = false;
+        state.addJob.error = action.error.message || "Failed to add job.";
+      })
+
+      // Fetch Jobs
+      .addCase(fetchJobs.pending, (state) => {
+        state.fetchJobs.loading = true;
+        state.fetchJobs.error = null;
+      })
+      .addCase(fetchJobs.fulfilled, (state, action) => {
+        state.fetchJobs.loading = false;
+        state.fetchJobs.jobs = action.payload;
+      })
+      .addCase(fetchJobs.rejected, (state, action) => {
+        state.fetchJobs.loading = false;
+        state.fetchJobs.error = action.error.message || "Failed to fetch jobs.";
+      })
   },
 });
 
+export const selectFetchJobs = (state: { jobsReducer: { fetchJobs: { jobs: FetchedJob[] } } }) =>
+  state.jobsReducer.fetchJobs.jobs;
 export default jobSlice.reducer;
