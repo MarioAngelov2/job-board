fde86d56fbb5872f3d2633d9ccb3cd1f7fd58631
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-04 16:59:40 +0200

    Minor refactor & transform data

--- server/src/services/getJobs.ts
+++ server/src/services/getJobs.ts
@@ -1,6 +1,44 @@
 import pool from "../database";
 import { SQL } from "sql-template-strings";
 
+const transformedRows = (rows: any) => {
+  return rows.map(
+    ({
+      job_title,
+      employment_type,
+      salary_range,
+      salary_type,
+      seniority_level,
+      seniority_type,
+      about_us,
+      date_posted,
+      final_words,
+      ...row
+    }: {
+      job_title: string,
+        employment_type: string,
+        salary_range: string,
+        salary_type: string,
+        seniority_level: string,
+        seniority_type: string,
+        about_us: string,
+        date_posted: string,
+        final_words: string,
+    }) => ({
+      jobTitle: job_title,
+      employmentType: employment_type,
+      salaryRange: salary_range,
+      salaryType: salary_type,
+      seniorityLevel: seniority_level,
+      seniorityType: seniority_type,
+      aboutUs: about_us,
+      datePosted: date_posted,
+      finalWords: final_words,
+      ...row,
+    })
+  );
+};
+
 export const fetchJobs = async () => {
   try {
     const jobs = await pool.query(SQL`SELECT 
@@ -17,15 +55,14 @@ export const fetchJobs = async () => {
     date_posted,
     tasks,
     final_words,
-    about_us,
     requirements,
     benefits,
-    images.url as image_url
+    images.url as logo
     FROM jobs
     INNER JOIN (SELECT job_id, url FROM images) AS images
     ON jobs.id = images.job_id`);
 
-    return jobs.rows;
+    return transformedRows(jobs.rows);
   } catch (error) {
     console.log(error);
     throw new Error("Database update error.");
