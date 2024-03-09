72d504f8ccded149fb4203be59f9b09337a71b01
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-09 11:02:27 +0200

    Create types file

---
+++ client/src/types/index.ts
@@ -0,0 +1,37 @@
+export type Job = {
+  jobTitle: string;
+  employmentType: string;
+  salaryRange: string;
+  salaryType: string;
+  seniorityLevel: string;
+  seniorityType: string;
+  aboutUs: string;
+  datePosted: string;
+  finalWords: string;
+  id: string;
+  company: string;
+  location: string;
+  tasks: string;
+  requirements: string[];
+  benefits: string[];
+  logo: string;
+};
+
+export type FetchedJob = {
+  jobTitle: string;
+  employmentType: string;
+  salaryRange: string;
+  salaryType: string;
+  seniorityLevel: string;
+  seniorityType: string;
+  aboutUs: string;
+  datePosted: string;
+  finalWords: string;
+  id: string;
+  company: string;
+  location: string;
+  tasks: string;
+  requirements: string[];
+  benefits: string[];
+  logo: string;
+};
