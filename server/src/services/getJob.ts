dee1aa15c2101b895d65a59c9f25e98c328597bf
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-09 10:55:06 +0200

    Transform rows before send to client

--- server/src/services/getJob.ts
+++ server/src/services/getJob.ts
@@ -1,6 +1,44 @@
 import pool from "../database";
 import { SQL } from "sql-template-strings";
 
+const transformedRows = (rows: any) => {
+  return rows.map(
+    ({
+      job_title,
+      employment_type,
+      salary_range,
+      salary_type,
+      seniority_level,
+      seniority_type,
+      about_us,
+      date_posted,
+      final_words,
+      ...row
+    }: {
+      job_title: string,
+        employment_type: string,
+        salary_range: string,
+        salary_type: string,
+        seniority_level: string,
+        seniority_type: string,
+        about_us: string,
+        date_posted: string,
+        final_words: string,
+    }) => ({
+      jobTitle: job_title,
+      employmentType: employment_type,
+      salaryRange: salary_range,
+      salaryType: salary_type,
+      seniorityLevel: seniority_level,
+      seniorityType: seniority_type,
+      aboutUs: about_us,
+      datePosted: date_posted,
+      finalWords: final_words,
+      ...row,
+    })
+  );
+}
+
 export const fetchJobById = async (id: string) => {
   try {
     const job = await pool.query(
@@ -25,7 +63,7 @@ export const fetchJobById = async (id: string) => {
       WHERE job_id = ${id}`
     );
 
-    return job.rows;
+    return transformedRows(job.rows);
   } catch (error) {
     console.log(error);
     throw new Error("Database update error.");
