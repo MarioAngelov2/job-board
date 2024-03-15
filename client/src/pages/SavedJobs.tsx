25bb4d11a563459286cd9ae79e8a05917b1b6821
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-15 12:46:14 +0200

    Update SavedJobs.tsx

--- client/src/pages/SavedJobs.tsx
+++ client/src/pages/SavedJobs.tsx
@@ -1,28 +1,30 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button } from "@/components/ui/button";
-
-const jobData = [
-  {
-    dateSaved: "2023-12-23",
-    jobTitle: "Software Engineer",
-    company: "Google",
-  },
-  {
-    dateSaved: "2024-01-10",
-    jobTitle: "Junior Front-end Developer",
-    company: "DXC",
-  },
-  {
-    dateSaved: "2024-01-22",
-    jobTitle: "FullStack Developer (Typescript, Node.JS, React)",
-    company: "WebThree",
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { fetchSavedJobs, selectSavedJobs } from "../redux/jobs/jobSlice";
+import { useEffect } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
 
 const SavedJobs = () => {
   const trimJobTitle = (title: string, maxLength: number) => {
     return title.length > maxLength ? title.slice(0, maxLength) + "..." : title;
   };
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const job = useSelector(selectSavedJobs);
+  const { userId } = useAuth();
+
+  useEffect(() => {
+    if (!userId) return;
+
+    dispatch(fetchSavedJobs(userId));
+  }, [dispatch, userId]);
+
+  console.log(job);
+
   return (
     <MaxWidthWrapper>
       <div className="flex flex-col min-h-screen px-2 py-4 md:py-12 md:px-8">
@@ -44,22 +46,24 @@ const SavedJobs = () => {
               <p className="text-gray-500">Company</p>
             </div>
           </div>
-          {jobData.map((job) => (
+          {job.map((job) => (
             <div
-              key={job.dateSaved}
+              key={job.jobId}
               className="flex flex-col gap-2 md:gap-8 md:flex-row mt-4 py-5 w-full border-b-[1px]"
             >
               <div className="flex w-full text-gray-500 md:justify-start">
-                {job.dateSaved}
+                {new Date(job.datePosted).toLocaleDateString()}
               </div>
-              <div className="flex w-full font-semibold cursor-pointer md:justify-start">
+              <div onClick={() => navigate(`/job/${job.jobId}`)} className="flex w-full font-semibold cursor-pointer md:justify-start">
                 {trimJobTitle(job.jobTitle, 35)}
               </div>
               <div className="flex w-full text-gray-500 cursor-pointer md:justify-start">
                 {job.company}
               </div>
               <div className="flex w-full text-gray-500 cursor-pointer md:justify-start">
-                <Button className="h-8 mt-3 md:mt-0" variant="destructive">Delete</Button>
+                <Button className="h-8 mt-3 md:mt-0" variant="destructive">
+                  Delete
+                </Button>
               </div>
             </div>
           ))}
