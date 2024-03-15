727cd1f231200bea35b1f44e457bd2da2a40570b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-15 14:31:48 +0200

    Update database.ts

--- server/src/database.ts
+++ server/src/database.ts
@@ -8,19 +8,18 @@ const pool = new Pool({
 
 pool.connect((err) => {
   if (err) throw err;
-  console.log("Connected to PostgreSQL successfully");
 
   const handleConnectionError = (err: any) => {
     console.error("Error connecting to PostgreSQL:", err.message);
   };
 
   setTimeout(() => {
-    console.log("Attempting to reconnect to PostgreSQL...");
+    console.log("Attempting to connect to PostgreSQL...");
     pool.connect((connectErr, client) => {
       if (connectErr) {
         handleConnectionError(connectErr); // Recursively handle reconnection errors
       } else {
-        console.log("Reconnected to PostgreSQL successfully");
+        console.log("Connected to PostgreSQL successfully");
         client.release(); // Release the client back to the pool
       }
     });
