d5a85f34d214728913226819e5b015f2a0496fef
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-17 11:42:21 +0200

    Create slicer for uploading file/cv

---
+++ client/src/redux/jobs/fileSlice.ts
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  file: null as string | null,
+  loading: false,
+  error: null as string | null,
+};
+
+
+export const uploadFile = createAsyncThunk(
+  "image/uploadImage",
+  async (data: any) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/files/uploadFile",
+        data
+      );
+
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+const fileSlice = createSlice({
+  name: "file",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(uploadFile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(uploadFile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.file = action.payload;
+      })
+      .addCase(uploadFile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to upload file.";
+      });
+  },
+});
+
+export const selectFile = (state: {
+  fileReducer: { file: string | null };
+}) => state.fileReducer.file
+
+export default fileSlice.reducer;
