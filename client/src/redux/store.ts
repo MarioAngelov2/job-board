9b43c1baed6d3a3dbeec829d0582fdae1243b498
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-17 11:42:24 +0200

    Update store.ts

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./filters/filterSlice";
 import jobsReducer from "./jobs/jobSlice";
 import imageReducer from "./jobs/imageSlice";
+import fileReducer from "./jobs/fileSlice";
 
 export const store = configureStore({
-  reducer: { filterReducer, jobsReducer, imageReducer },
+  reducer: { filterReducer, jobsReducer, imageReducer, fileReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
