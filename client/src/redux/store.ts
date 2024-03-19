6451ae843410fe25f9910e9c41832fd7dbd6e5cd
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-19 07:54:25 +0200

    Add application reducer

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -3,9 +3,16 @@ import filterReducer from "./filters/filterSlice";
 import jobsReducer from "./jobs/jobSlice";
 import imageReducer from "./jobs/imageSlice";
 import fileReducer from "./jobs/fileSlice";
+import applicationReducer from "./jobs/applicationSlice";
 
 export const store = configureStore({
-  reducer: { filterReducer, jobsReducer, imageReducer, fileReducer },
+  reducer: {
+    filterReducer,
+    jobsReducer,
+    imageReducer,
+    fileReducer,
+    applicationReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
