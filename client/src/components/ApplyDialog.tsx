566c5296a55af8dc7b168a71bb1ff427d36ba19f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-19 07:55:33 +0200

    Apply for a job logic

--- client/src/components/ApplyDialog.tsx
+++ client/src/components/ApplyDialog.tsx
@@ -22,35 +22,62 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { toast } from "react-hot-toast";
+import { uploadFile, selectFile } from "../redux/jobs/fileSlice";
+import { applyJob } from "../redux/jobs/applicationSlice";
+import { AppDispatch } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth } from "@clerk/clerk-react";
 
 type ApplyDialogProps = {
   className?: string;
   children?: any;
+  jobId: string | undefined;
 };
 
 const formSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  uploadFile: z.string(),
+  file: z.object({ file: z.any() }),
 });
 
-const ApplyDialog = ({ className, children }: ApplyDialogProps) => {
+const ApplyDialog = ({ className, children, jobId }: ApplyDialogProps) => {
   const [open, setOpen] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       email: "",
-      uploadFile: "",
+      file: undefined,
     },
   });
+  const dispatch = useDispatch<AppDispatch>();
+  const userCV = useSelector(selectFile);
+  const { userId } = useAuth();
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    if (!jobId) return;
+
+    if (typeof userCV === "string" || !userCV) return
 
     setOpen(false);
     toast.success("Your CV has been sent successfully!");
     form.reset();
+    const formData = {
+      ...values,
+      jobId,
+      userId,
+      userCV,
+    };
+    dispatch(applyJob(formData));
+  };
+
+  const handleUploadFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append("file", file);
+      dispatch(uploadFile(formData));
+    }
   };
 
   return (
@@ -114,15 +141,17 @@ const ApplyDialog = ({ className, children }: ApplyDialogProps) => {
             />
             <FormField
               control={form.control}
-              name="uploadFile"
-              render={({ field }) => (
+              name="file"
+              render={() => (
                 <FormItem className="flex flex-col mt-3 md:flex-col md:items-start">
                   <FormLabel>Attach CV</FormLabel>
                   <FormControl>
                     <Input
                       type="file"
                       className="w-full sm:min-w-[350px] cursor-pointer text-slate-500"
-                      {...field}
+                      {...form.register("file", {
+                        onChange: (e) => handleUploadFile(e),
+                      })}
                     />
                   </FormControl>
                   <FormMessage />
