769c61d8f04f8abae86c324e626144e182aa58be
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-20 08:15:32 +0200

    Update EmptySavedJobs.tsx

--- client/src/components/EmptySavedJobs.tsx
+++ client/src/components/EmptySavedJobs.tsx
@@ -4,12 +4,14 @@ import { useNavigate } from "react-router-dom";
 const EmptySavedJobs = () => {
   const navigate = useNavigate();
 
-  return (
-    <div className="flex flex-col items-center min-h-screen mt-48">
-      <h1 className="text-3xl">No added jobs yet</h1>
-      <p className="mt-1">Save jobs to view them later</p>
-      <Button onClick={() => navigate('/')} className="mt-6">Browse here</Button>
-    </div>
+  return ( 
+        <div className="flex flex-col items-center justify-center min-h-screen px-2 py-4 md:py-12 md:px-8">
+          <h1 className="mb-1 text-xl font-black text-gray-600 uppercase md:text-2xl">
+            No added jobs yet
+          </h1>
+          <p className="mb-4 text-gray-600">Save jobs to view them later</p>
+          <Button onClick={() => navigate("/")}>Browse here</Button>
+        </div>
   );
 };
 
