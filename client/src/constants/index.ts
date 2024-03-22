526c12c8962f18719dc459ce05527cf787f91b2c
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-03-22 08:46:50 +0200

    Update index.ts

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,4 +1,4 @@
-export const seniorityOptions = [
+const seniorityOptions = [
   { value: "intern", label: "Intern" },
   { value: "junior", label: "1-2 years experience" },
   { value: "mid", label: "2-5 years experience" },
@@ -14,4 +14,4 @@ if (import.meta.env.VITE_NODE_ENV === "development") {
   URL = import.meta.env.VITE_PRODUCTION_ORIGIN;
 }
 
-export { URL };
+export { URL, seniorityOptions };
